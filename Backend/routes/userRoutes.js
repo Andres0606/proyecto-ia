743358c938bafa2a1c5035247a2e088e0833ce68
Andres0606 <andres.suarezro@campusucc.edu.co>
2026-05-14 11:27:32 -0500

    arreglo a plan

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -23,7 +23,7 @@ router.get('/get-cv-url/:userId', fileController.getResumeUrl);
 // Rutas de gestión de perfil completo
 router.get('/profile/:userId', userController.getFullProfile);
 router.put('/profile/:userId', userController.updateProfile);
-router.post('/update-plan', userController.updatePlan);
-router.post('/subscribe', userController.subscribe);
+router.put('/update-plan', userController.updatePlan);
+router.put('/subscribe', userController.subscribe);
 
 module.exports = router;
